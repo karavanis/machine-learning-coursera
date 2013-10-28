46d97ec87cc97901a390319ba9d1ae0f60ac4f66
Konstantinos Aravanis <kos.arav@gmail.com>
2013-10-29 00:08:12 +0100

    gradientDescentMulti

--- mlclass-ex1/mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/mlclass-ex1/gradientDescentMulti.m
@@ -8,30 +8,10 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+    theta = theta - (alpha / m)  * ((theta' * X' - y') * X)';
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
-
-
-
-
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
-
 end
 
 end
