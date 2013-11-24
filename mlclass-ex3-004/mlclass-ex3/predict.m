ed1b8a1b29ceb2945440d0eee67bddec6a2e6768
Konstantinos Aravanis <kos.arav@gmail.com>
2013-11-24 23:34:48 +0100

    predict

--- mlclass-ex3-004/mlclass-ex3/predict.m
+++ mlclass-ex3-004/mlclass-ex3/predict.m
@@ -5,31 +5,11 @@ function p = predict(Theta1, Theta2, X)
 
 % Useful values
 m = size(X, 1);
-num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+a2 = sigmoid(X * Theta1');
 
-
-
-
-
-
-
-
-
-% =========================================================================
-
-
+a2 = [ones(m, 1) a2];
+[max_value, p] = max(sigmoid(a2 * Theta2'), [], 2);
 end
