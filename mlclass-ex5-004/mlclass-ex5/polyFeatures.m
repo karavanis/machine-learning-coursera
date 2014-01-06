887baac1ad3691f8ce2348642f4d3be1c3a777e5
Konstantinos Aravanis <kos.arav@gmail.com>
2014-01-06 16:12:16 +0100

    polyFeatures

--- mlclass-ex5-004/mlclass-ex5/polyFeatures.m
+++ mlclass-ex5-004/mlclass-ex5/polyFeatures.m
@@ -9,17 +9,7 @@ function [X_poly] = polyFeatures(X, p)
 % You need to return the following variables correctly.
 X_poly = zeros(numel(X), p);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
-%               column of X contains the values of X to the p-th power.
-%
-% 
-
-
-
-
-
-
-% =========================================================================
-
+for i = 1:p
+  X_poly(:, i) = X .^ i;
+end
 end
