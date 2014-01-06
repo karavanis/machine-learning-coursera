a59174e09d85af7ef84bc3e8682d05c4c05af951
Konstantinos Aravanis <kos.arav@gmail.com>
2014-01-06 16:25:21 +0100

    validationCurve

--- mlclass-ex5-004/mlclass-ex5/validationCurve.m
+++ mlclass-ex5-004/mlclass-ex5/validationCurve.m
@@ -16,38 +16,12 @@ lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
-%               lambda = lambda_vec(i)
-%
-% Note: You can loop over lambda_vec with the following:
-%
-%       for i = 1:length(lambda_vec)
-%           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
-%           % regression with regularization parameter lambda
-%           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
-%       end
-%
-%
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X, y, lambda);
 
+  error_train(i) = linearRegCostFunction(X,    y,    theta, 0);
+  error_val(i)   = linearRegCostFunction(Xval, yval, theta, 0);
 
-
-
-
-
-
-
-
-
-% =========================================================================
-
+end
 end
