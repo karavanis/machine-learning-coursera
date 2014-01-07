2394fb136cf175106f16a53ce28fd4016a3cb6d1
Konstantinos Aravanis <kos.arav@gmail.com>
2014-01-07 22:44:22 +0100

    dataset3Params

--- mlclass-ex6-004/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-004/mlclass-ex6/dataset3Params.m
@@ -2,33 +2,28 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+num_of_values = numel(values);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+% set the value to a really high error that is impossible to reach
+smallest_error = 10^10000;
 
+for i = 1:num_of_values
+  for j = 1:num_of_values
+    model = svmTrain(X, y, values(i), @(x1, x2) gaussianKernel(x1, x2, values(j)));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
 
-
-
-
-
-
-% =========================================================================
-
+    if error < smallest_error
+      smallest_error = error;
+      C = values(i);
+      sigma = values(j);
+    end
+  end
+end
 end
