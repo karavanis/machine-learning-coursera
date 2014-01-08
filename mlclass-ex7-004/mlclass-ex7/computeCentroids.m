d23ea58b3f64a6aa7752ae8555091181f755d9b4
Konstantinos Aravanis <kos.arav@gmail.com>
2014-01-08 22:36:48 +0100

    computeCentroids

--- mlclass-ex7-004/mlclass-ex7/computeCentroids.m
+++ mlclass-ex7-004/mlclass-ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returs the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -16,25 +16,15 @@ function centroids = computeCentroids(X, idx, K)
 % You need to return the following variables correctly.
 centroids = zeros(K, n);
 
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every centroid and compute mean of all points that
-%               belong to it. Concretely, the row vector centroids(i, :)
-%               should contain the mean of the data points assigned to
-%               centroid i.
-%
-% Note: You can use a for-loop over the centroids to compute this.
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
-
+for i = 1:K
+  count = 0;
+  for j = 1:m
+    if idx(j) == i
+      centroids(i, :) = centroids(i, :) + X(j, :);
+      count++;
+    end
+  end
+  centroids(i, :) = centroids(i, :) ./ count;
+end
 end
 
